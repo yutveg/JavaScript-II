01cde4e4707e3571545630f25a3ea98350adaada
yutveg <kodi.0n31l@gmail.com>
2019-10-15 13:42:55 -0700

    mvp completo

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,8 +3,19 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
-
-
+function baseLevel() {
+  let testvar = 'Base Level Access';
+  function closureExample() {
+    let testvar1 = 'Closure Level Access';
+    console.log(`closure function has ${testvar}`);
+    console.log(`closure function has ${testvar1}`);
+  }
+  closureExample();
+  console.log(`baseLevel function has ${testvar}`);
+  console.log(`closure function has ${testvar1}`);
+  
+}
+console.log(baseLevel());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
